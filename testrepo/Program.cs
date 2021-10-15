e0e180f7ba345ec8ea6061bdafb4b5cd960b80d5
dennis <dennis.barber@microfocus.com>
2021-10-15 11:39:06 -0400

    User:dennis Branch:main Commit:0

---
+++ testrepo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testrepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Main m = new Main();
+            m.Method0();
+        }
+    }
+}
