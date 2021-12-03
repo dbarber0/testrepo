043c5c6cff897b703ccd3bfd0b12a3608a5d5a43
bob <dbarber042@gmail.com>
2021-12-03 14:50:10 -0500

    User:Bob Branch:Main Commit:4

--- testrepo/Main.cs
+++ testrepo/Main.cs
@@ -19,6 +19,7 @@ namespace testrepo
             //  User:bob    Branch:main Commit:1
             //  User:bob    Branch:main Commit:2
             //  User:bob    Branch:main Commit:3
+            //  User:bob    Branch:main Commit:4
         }
     }
 }
